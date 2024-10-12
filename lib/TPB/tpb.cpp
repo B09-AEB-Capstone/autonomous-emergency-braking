452ade5d54426795c4040347ac0e45af5f712089
inamnurul <aam.inam7310@gmail.com>
2024-10-12 10:16:37 +0700

    add: tpb loogic function

---
+++ lib/TPB/tpb.cpp
@@ -0,0 +1,17 @@
+#include "TPB.h"
+#include <vector>
+
+TPB::TPB() {}
+
+float TPB::calculateTPB(float ttc, const std::vector<float>& apb_section, float relative_speed) {
+    float apb = 0;
+
+    for (float apb_value : apb_section) {
+        if (relative_speed / apb_value > ttc) {
+            apb = apb_value;
+            break; 
+        }
+    }
+
+    return apb;
+}
---
+++ lib/TPB/tpb.h
@@ -0,0 +1,9 @@
+#ifndef TPB_H
+#define TPB_H
+
+class TPB {
+  public:
+    float calculateTPB(float ttc, const std::vector<float>& apb_section, float relative_speed);  
+};
+
+#endif
